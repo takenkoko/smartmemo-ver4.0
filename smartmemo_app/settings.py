f1e240c33ad10c3cb43ada7f98856ba0e702b5f9
Takeshi Satake <takeshi.satake.dev@gmail.com>
2026-08-05 22:02:06 +0900

    Add password reset functionality with email support

--- smartmemo_app/settings.py
+++ smartmemo_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +128,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-#メール送信なしコンソール表示設定を追加
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#コンソール表示ではなく、実際にSMTP経由で送信する設定
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'noreply@smartmemo.local'
